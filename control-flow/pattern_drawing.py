b87c9bb9f898e8d16abe42e1a495e0411c1f014d
Saladeen Mustapha <mustapha.saladeen6@gmail.com>
2024-12-25 21:21:11 +0000

    pattern draqing script

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,9 @@
+pattern_length = int(input("Enter the size of the pattern: "))
+column = pattern_length
+
+for l in range(0, pattern_length):
+    for j in range(pattern_length):
+        print("*", end="")
+    print()
+    l += 1
+        
